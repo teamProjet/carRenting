94803aad087bb6787093a8370322aced64b9ee94
Keany ALBERTINI <keanyalbertini@gmail.com>
2022-01-11 14:38:15 +0100

    add catalog page with one filter and link to one car

---
+++ public/js/scripts.js
@@ -0,0 +1,24 @@
+// search for catalog by title
+
+function search_car() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('h3_catalog');
+
+	let z = document.getElementsByClassName('card_catalog');
+      
+    for (i = 0; i < x.length; i++) { console.log(x.length);
+        
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            
+                z[i].style.display="none";
+            
+            
+        }
+        else {
+            z[i].style.display="flex";                 
+        }
+    }
+}
+
+// price filter
